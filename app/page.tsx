bc6fdce6ccb384dc905a1e915694765d61cff1ea
Ryan Biondo <biondoryan.dev@gmail.com>
2023-11-07 22:24:09 -0700

    Optimizations

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,39 @@
+'use client';
 import Image from 'next/image';
 import napoleon from '@/public/images/napoleon.gif';
+import { Metadata } from 'next';
+import { useState } from 'react';
 
 export default function Home() {
   return (
     <>
-      <main className="relative h-screen">
+      <main>
+        <h1>Hello World</h1>
+        <button
+          onClick={async () => {
+            const _ = (await import('lodash')).default;
+            const users = [{ name: 'c' }, { name: 'b' }, { name: 'a' }];
+            _.orderBy(users, ['name']);
+          }}>
+          Show
+        </button>
         <Image
-          src="https://bit.ly/react-cover"
-          alt="cloudimg"
-          fill
-          className="object-cover"
-          sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-          quality={75}
-          priority
+          src={napoleon}
+          alt="hero"
+          width={200}
+          height={200}
+          style={{ zIndex: 100 }}
         />
       </main>
-      <Image
-        src={napoleon}
-        alt="hero"
-        width={200}
-        height={200}
-        style={{ zIndex: 100 }}
-      />
     </>
   );
+}
+
+export async function generateMetadata(): Promise<Metadata> {
+  const product = await fetch('https://fakestoreapi.com/products/1');
+
+  return {
+    title: 'product.title',
+    description: 'product.description',
+  };
 }
